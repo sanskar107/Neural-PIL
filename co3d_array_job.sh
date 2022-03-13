9e7da86cd26aefbd89666f6b11b9fa0bcbae224c
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2022-03-13 00:35:51 -0800

    add new co3d scenes

--- co3d_array_job.sh
+++ co3d_array_job.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
-#SBATCH --job-name=co3d_npill
+#SBATCH --job-name=co3d_npil_new
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
 #SBATCH --qos=normal
 #SBATCH -c 8
 #SBATCH --output="./co3d_logs/co3d-%a-%j.log"
 #SBATCH --open-mode=append
-#SBATCH --array="0-10"
+#SBATCH --array="0-3"
+
+# scenes=(
+# backpack_373_41833_83497
+# handbag_396_49743_97969
+# handbag_399_51220_100573
+# hydrant_106_12677_24990
+# hydrant_268_28513_58093
+# motorcycle_362_38283_75358
+# plant_253_27235_55344
+# plant_372_40884_81286
+# toytruck_353_37431_70460
+# toytruck_379_44672_89080
+# vase_380_44868_89574
+# )
 
 scenes=(
-backpack_373_41833_83497
-handbag_396_49743_97969
-handbag_399_51220_100573
-hydrant_106_12677_24990
-hydrant_268_28513_58093
-motorcycle_362_38283_75358
-plant_253_27235_55344
-plant_372_40884_81286
-toytruck_353_37431_70460
-toytruck_379_44672_89080
-vase_380_44868_89574
+bottle_44_2232_6712
+parkingmeter_425_59343_114810
+toaster_372_41229_82130
+vase_149_16568_31914
 )
 
 scene="${scenes[$SLURM_ARRAY_TASK_ID]}"
